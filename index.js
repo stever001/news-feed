5fd49a0e5f3da38051fa91a675f4269fa0331016
SteveR <steve17rouse@gmail.com>
2024-02-26 15:26:41 -0700

    corrected catch placement with proper chaining

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+const articles = []
+
+app.get('/', (req, res) => {res.json(`Welcome to my News API`)
+
+})
+
+//scraper
+app.get('/news', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+    .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = [];
+
+        $('a:contains("climate")', html).each(function() {
+            const title = $(this).text();
+            const url = $(this).attr('href');
+            articles.push({ title, url });
+        });
+
+        res.json(articles);
+    })
+    .catch((err) => console.log(err)); // Corrected catch placement and error logging
+});
+
 app.listen(PORT, () => console.log(`server listening on PORT ${PORT}`))
